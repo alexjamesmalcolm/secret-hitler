ee645a6951c610f1187b46ecb69811f9241abcf5
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-10-21 15:49:12 -0400

    Created controller Probably will stop using thymeleaf though and elimate the need for mvc

---
+++ src/main/java/com/alexjamesmalcolm/secrethitler/LobbyController.java
@@ -0,0 +1,13 @@
+package com.alexjamesmalcolm.secrethitler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LobbyController {
+	
+	@RequestMapping("/")
+	public String loginView() {
+		return "index";
+	}
+}
