55095929f6464d1826458735db0501c2e950a070
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-10-21 12:37:53 -0400

    Started project using Spring Initializr

---
+++ src/main/java/com/alexjamesmalcolm/secrethitler/SecrethitlerApplication.java
@@ -0,0 +1,12 @@
+package com.alexjamesmalcolm.secrethitler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecrethitlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecrethitlerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexjamesmalcolm/secrethitler/SecrethitlerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alexjamesmalcolm.secrethitler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecrethitlerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
