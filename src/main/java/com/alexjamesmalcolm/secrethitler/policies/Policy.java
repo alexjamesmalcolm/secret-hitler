def3815ad349991f7dffd605ed42c0349725a47c
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-12-13 21:09:21 -0500

    Can I get a small McFix with that order?

--- src/main/java/com/alexjamesmalcolm/secrethitler/policies/Policy.java
+++ src/main/java/com/alexjamesmalcolm/secrethitler/policies/Policy.java
@@ -1,11 +1,13 @@
 package com.alexjamesmalcolm.secrethitler.policies;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public abstract class Policy {
 
     @Id
+    @GeneratedValue
     private long id;
 }
