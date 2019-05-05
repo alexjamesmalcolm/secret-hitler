9c0e12f840bca77b96abedbab3a17b57defbf835
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2019-05-05 02:41:22 -0400

    Created a startGame endpoint

--- src/main/java/com/alexjamesmalcolm/secrethitler/GameSocketController.java
+++ src/main/java/com/alexjamesmalcolm/secrethitler/GameSocketController.java
@@ -1,8 +1,23 @@
 package com.alexjamesmalcolm.secrethitler;
 
+import com.alexjamesmalcolm.secrethitler.game.Game;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-@Controller
+import javax.annotation.Resource;
+
+@Controller("/out")
 public class GameSocketController {
-    
+
+    @Resource
+    private GameRepository gameRepo;
+
+    @MessageMapping("/start-game")
+    @SendTo("/game")
+    public Game startGame() {
+        Game game = new Game();
+        game = gameRepo.save(game);
+        return game;
+    }
 }
